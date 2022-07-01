601597187299efb030248d31607a7a509a5eb45b
Tishkina <iv.tishkina@gmail.com>
2022-07-01 09:02:54 +0400

    Add files via upload

---
+++ 4.4.3 Функции.py
@@ -0,0 +1,11 @@
+# Разбиение программы на функции
+
+def cnk(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - K))
+print(cnk)
+
+def max2(a, b):
+    if a > b:
+        return a
+    else:
+        return b
